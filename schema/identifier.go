6660177749f02dc11ac723e3e33970e4b51596f6
brct-james <james.clague@brct.io>
2022-07-17 14:56:46 +0000

    Remove existing code

--- schema/identifier.go
+++
@@ -1,8 +0,0 @@
-// Package schema defines database and JSON schema as structs, as well as functions for creating and using these structs
-package schema
-
-// Defines an Identifier
-type Identifier struct {
-	Name string `yaml:"Name" json:"name" binding:"required"`
-	Symbol string `yaml:"Symbol" json:"symbol" binding:"required"`
-}
