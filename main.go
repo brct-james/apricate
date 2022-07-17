e84ad3c95957714c7c6451a4f9a11cfcd0c67129
brct-james <james.clague@brct.io>
2022-07-17 17:38:01 +0000

    Finish copying old function load_config()

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 // TODO: Test
-func LoadEnvToLines(env_path string) []string {
+func LoadEnvFileToLines(env_path string) []string {
 	// Ensure exists
 	filemngr.Touch(env_path)
 	// Load config file
@@ -16,6 +16,16 @@ func LoadEnvToLines(env_path string) []string {
 		log.Error.Fatalf("Could not read lines from %s. Err: %v", env_path, readErr)
 	}
 	return lines
+}
+
+// TODO: Test
+func WriteEnvLinesToFile(env_path string, lines []string) {
+	// Join and write out
+	writeErr := filemngr.WriteLinesToFile(env_path, lines)
+	if writeErr != nil {
+		log.Error.Fatalf("Could not write %s: %v", env_path, writeErr)
+	}
+	log.Info.Printf("Wrote config to %s", env_path)
 }
 
 func HandleServerConfigFlushDBs(lines []string) []string {
@@ -44,20 +54,54 @@ func HandleServerConfigFlushDBs(lines []string) []string {
 	} else {
 		log.Debug.Printf("flush_dbs neither true nor dev, skipping flush")
 	}
-	// Update secret
+	// Update
 	if flush_dbs != "dev" {
 		lines[fdb_lines_index] = "flush_dbs=false"
 	}
 	return lines
 }
 
+func HandleServerConfigRegenerateAuthSecret(lines []string) []string {
+	ras_lines_index, regenerate_auth_secret := filemngr.GetKeyFromLines("regenerate_auth_secret", lines)
+	if regenerate_auth_secret == "" {
+		log.Important.Printf("regenerate_auth_secret not found in server_config.env, creating with default value of false")
+		// Create secret in env file since could not find one to update
+		// If empty file then replace 1st line else append to end
+		log.Debug.Printf("Creating new secret in env file. server_config.env[0] == ''? %v", lines[0] == "")
+		if lines[0] == "" {
+			log.Debug.Printf("Line 0 empty in server_config.env, replacing line 0")
+			lines[0] = "regenerate_auth_secret=false"
+			ras_lines_index = 0
+			regenerate_auth_secret = "false"
+		} else {
+			log.Debug.Printf("Not blank server_config.env, appending to end")
+			lines = append(lines, "regenerate_auth_secret=false")
+			ras_lines_index = len(lines)-1
+			regenerate_auth_secret = "false"
+		}
+	}
+	if regenerate_auth_secret == "true" {
+		// regen
+		log.Important.Printf("regenerate_auth_secret = %s, flushing", regenerate_auth_secret)
+		//TODO
+		// Update
+		lines[ras_lines_index] = "regenerate_auth_secret=false"
+	} else {
+		log.Debug.Printf("regenerate_auth_secret neither true nor dev, skipping flush")
+	}
+	return lines
+}
+
 func ProcessServerConfig(lines []string) []string {
 	lines = HandleServerConfigFlushDBs(lines)
+	lines = HandleServerConfigRegenerateAuthSecret(lines)
+
 	return lines
 }
 
 func main() {
 	log.Important.Printf("Hello, World")
-	server_config_lines := LoadEnvToLines("data/server_config.env")
-	ProcessServerConfig(server_config_lines)
+	server_config_lines := LoadEnvFileToLines("data/server_config.env")
+	server_config_lines = ProcessServerConfig(server_config_lines)
+	WriteEnvLinesToFile("data/server_config.env", server_config_lines)
 }
--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 func TestHandleServerConfigFlushDBs(t *testing.T) {
 	t.Run("return flush_dbs=false when initially true", func(t *testing.T) {
 		lines := []string{"flush_dbs=true"}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"flush_dbs=false"}
 
 		if !comparison.StringSlicesEqual(got, want) {
@@ -18,7 +18,7 @@ func TestHandleServerConfigFlushDBs(t *testing.T) {
 	})
 	t.Run("return flush_dbs=dev when initially dev", func(t *testing.T) {
 		lines := []string{"flush_dbs=dev"}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"flush_dbs=dev"}
 
 		if !comparison.StringSlicesEqual(got, want) {
@@ -27,7 +27,7 @@ func TestHandleServerConfigFlushDBs(t *testing.T) {
 	})
 	t.Run("return flush_dbs=false when initially false", func(t *testing.T) {
 		lines := []string{"flush_dbs=dev"}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"flush_dbs=dev"}
 
 		if !comparison.StringSlicesEqual(got, want) {
@@ -36,7 +36,7 @@ func TestHandleServerConfigFlushDBs(t *testing.T) {
 	})
 	t.Run("return flush_dbs=false appended when flush_dbs missing", func(t *testing.T) {
 		lines := []string{"other_key=boring"}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"other_key=boring", "flush_dbs=false"}
 
 		if !comparison.StringSlicesEqual(got, want) {
@@ -45,7 +45,7 @@ func TestHandleServerConfigFlushDBs(t *testing.T) {
 	})
 	t.Run("return flush_dbs=false inserted when flush_dbs missing and first line empty", func(t *testing.T) {
 		lines := []string{""}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"flush_dbs=false"}
 
 		if !comparison.StringSlicesEqual(got, want) {
@@ -54,8 +54,122 @@ func TestHandleServerConfigFlushDBs(t *testing.T) {
 	})
 	t.Run("return flush_dbs=false inserted when flush_dbs missing and first line empty", func(t *testing.T) {
 		lines := []string{"", "other_key=boring"}
-		got := ProcessServerConfig(lines)
+		got := HandleServerConfigFlushDBs(lines)
 		want := []string{"flush_dbs=false", "other_key=boring"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+}
+
+func TestHandleServerConfigRegenerateAuthSecret(t *testing.T) {
+	t.Run("return regenerate_auth_secret=false when initially true", func(t *testing.T) {
+		lines := []string{"regenerate_auth_secret=true"}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("return regenerate_auth_secret=dev when initially dev", func(t *testing.T) {
+		lines := []string{"regenerate_auth_secret=dev"}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"regenerate_auth_secret=dev"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("return regenerate_auth_secret=false when initially false", func(t *testing.T) {
+		lines := []string{"regenerate_auth_secret=dev"}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"regenerate_auth_secret=dev"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("return regenerate_auth_secret=false appended when regenerate_auth_secret missing", func(t *testing.T) {
+		lines := []string{"other_key=boring"}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"other_key=boring", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("return regenerate_auth_secret=false inserted when regenerate_auth_secret missing and first line empty", func(t *testing.T) {
+		lines := []string{""}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("return regenerate_auth_secret=false inserted when regenerate_auth_secret missing and first line empty", func(t *testing.T) {
+		lines := []string{"", "other_key=boring"}
+		got := HandleServerConfigRegenerateAuthSecret(lines)
+		want := []string{"regenerate_auth_secret=false", "other_key=boring"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+}
+
+func TestProcessServerConfig(t *testing.T) {
+	t.Run("empty config file", func(t *testing.T) {
+		lines := []string{""}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=false", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("flush=dev regenerate=false", func(t *testing.T) {
+		lines := []string{"flush_dbs=dev", "regenerate_auth_secret=false"}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=dev", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("flush=dev regenerate=true", func(t *testing.T) {
+		lines := []string{"flush_dbs=dev", "regenerate_auth_secret=true"}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=dev", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("flush=true regenerate=false", func(t *testing.T) {
+		lines := []string{"flush_dbs=true", "regenerate_auth_secret=false"}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=false", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("flush=false regenerate=true", func(t *testing.T) {
+		lines := []string{"flush_dbs=false", "regenerate_auth_secret=true"}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=false", "regenerate_auth_secret=false"}
+
+		if !comparison.StringSlicesEqual(got, want) {
+			t.Errorf("lines %q, got %q, want %q", lines, got, want)
+		}
+	})
+	t.Run("flush=true regenerate=true", func(t *testing.T) {
+		lines := []string{"flush_dbs=true", "regenerate_auth_secret=true"}
+		got := ProcessServerConfig(lines)
+		want := []string{"flush_dbs=false", "regenerate_auth_secret=false"}
 
 		if !comparison.StringSlicesEqual(got, want) {
 			t.Errorf("lines %q, got %q, want %q", lines, got, want)
